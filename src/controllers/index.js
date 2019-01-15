dfa45d501064ca3d310782ae0ba5459bc518c144
unknown <bongthoi1111@gmail.com>
2019-01-15 10:12:27 +0700

    2.1- update check errors from api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -26,7 +26,15 @@ router.get("/user/list", async(req, res) => {
 	try {
 		result=await userService.getAll();
 		/* console.log("list="+JSON.stringify(result)); */
-		res.render("list", { title: "User List", users:result.data });
+
+		if(result.status=="success"){
+			console.log(method+" -->success");
+			res.render("list", { title: "User List", users:result.data });
+		}else{
+			console.log(method+" -->failed from the api");
+			res.render("list", { title: "User List", users:result });
+		}		
+		
 	} catch (error) {
 		res.render("list", { title: "User List", users:result });
 	}
@@ -62,8 +70,14 @@ router.post("/user/add",async(req,res)=>{
 		try {
 			let result=await userService.insert(user);
 
-			console.log(method+" -->success");
-			res.redirect("/user/list");
+			if(result.status=="success"){
+				console.log(method+" -->success");
+				res.redirect("/user/list");
+			}else{
+				console.log(method+" -->failed from api");
+				res.render("form_add", { title: "add User"});			
+			}
+
 		} catch (error) {
 			console.log(method+" -->fail");
 			res.render("form_add", { title: "add User"});			
@@ -78,8 +92,14 @@ router.get("/user/update/:_userID",async(req,res)=>{
 	try {
 		let result= await userService.getUserByID(req.params._userID);
 
-		console.log(method+" -->success"+JSON.stringify(result));
-		res.render("form_edit", { title: "Edit User",user:result.data});	
+		if(result.status=="success"){
+			console.log(method+" -->success"+JSON.stringify(result));
+			res.render("form_edit", { title: "Edit User",user:result.data});	
+		}else{
+			console.log(method+" -->failed from api");
+			res.render("form_edit", { title: "Edit User",user:null});
+		}
+
 	} catch (error) {
 		console.log(method+" -->fail");
 		res.render("form_edit", { title: "Edit User",user:null});				
@@ -94,8 +114,14 @@ router.post("/user/update/:_userID",async(req,res)=>{
 		let user=new User(req.params._userID,req.body.username,req.body.email,req.body.phone);
 		let result= await userService.update(user);
 
-		console.log(method+" -->success");
-		res.redirect("/user/list");
+		if(result.status=="success"){
+			console.log(method+" -->success");
+			res.redirect("/user/list");
+		}else{
+			console.log(method+" -->failed from api");
+			res.redirect("/user/list");			
+		}
+		
 	} catch (error) {
 		console.log(method+" -->fail");
 		res.redirect("/user/list");			
@@ -109,10 +135,16 @@ router.get("/user/delete/:_userID",async(req,res)=>{
 	try {
 		let result= await userService.delete(req.params._userID);
 		
-		console.log(method+" -->success");
-		res.redirect("/user/list");
+		if(result.status=="success"){
+			console.log(method+" -->success");
+			res.redirect("/user/list");
+		}else{
+			console.log(method+" -->failed from api");
+			res.redirect("/user/list");			
+		}
+		
 	} catch (error) {
-		console.log(method+" -->fail");
+		console.log(method+" -->failed");
 		res.redirect("/user/list");			
 	}
 });
@@ -125,8 +157,14 @@ router.post("/user/search", async(req, res) => {
 	try {
 		result=await userService.getUserByName(req.body.keyword);
 
-		console.log(method+" -->success");
-		res.render("list", { title: "User List", users:result.data});
+		if(result.status=="success"){
+			console.log(method+" -->success");
+			res.render("list", { title: "User List", users:result.data});
+		}else{
+			console.log(method+" -->failed from api");
+			res.render("list", { title: "User List",users:result });
+		}
+		
 	} catch (error) {
 		console.log(method+" -->fail");
 		res.render("list", { title: "User List",users:result });
